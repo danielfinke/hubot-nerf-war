9a971df8aa39c95b1df82d400f150ae10bfab75f
Daniel Finke <danielfinke2011@gmail.com>
2018-01-03 20:42:28 -0800

    README/index.js/.gitignore

---
+++ README.md
@@ -0,0 +1,30 @@
+# hubot-random-events
+
+Hubot script for scheduling events to be performed at a random time within a window
+
+See [`index.js`](index.js) for full documentation.
+
+## Installation
+
+In hubot project repo, run:
+
+`npm install hubot-random-events --save`
+
+Then add **hubot-random-events** to your `external-scripts.json`:
+
+```json
+[
+  "hubot-random-events"
+]
+```
+
+## Sample Interaction
+
+```
+user1>> hubot hello
+hubot>> hello!
+```
+
+## NPM Module
+
+https://www.npmjs.com/package/hubot-random-events
---
+++ index.js
@@ -0,0 +1,29 @@
+// Description
+//   Hubot script for scheduling events to be performed at a random time within a window
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot hello - <what the respond trigger does>
+//   orly - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Daniel Finke <danielfinke2011@gmail.com>
+
+module.exports = (robot) => {
+    robot.respond(/list random events/i, res => {
+
+    });
+
+    robot.respond(/schedule random event/i, res => {
+
+    });
+
+    robot.respond(/delete random event/i, res => {
+
+    });
+};
